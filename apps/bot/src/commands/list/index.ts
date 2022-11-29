cb61818f7c16d4f8471c67fd2426ce8ee5c2dd02
Ismael Trentin <ismael.trentin@samtrevano.ch>
2022-11-29 01:33:39 +0100

    remove non implemented subcommand

--- apps/bot/src/commands/list/index.ts
+++ apps/bot/src/commands/list/index.ts
@@ -1,42 +1,34 @@
-import { Command } from '../../@types';
+import { CommandInteractionHandler } from '../../@types';
 import { SlashCommandBuilder } from 'discord.js';
 import { listAll } from './list.all';
 
-const list: Command = {
+const list: CommandInteractionHandler = {
   data: new SlashCommandBuilder()
     .setName('list')
     .setDescription('Claimed characters list related commands')
     .addSubcommand(cmd =>
       cmd.setName('all').setDescription('List all your claimed characters')
-    )
-    .addSubcommand(cmd =>
-      cmd
-        .setName('view')
-        .setDescription('View a specific character')
-        .addIntegerOption(opt =>
-          opt
-            .setName('localid')
-            .setDescription(
-              'The character local id, aka its position in the list'
-            )
-            .setMinValue(1)
-            .setRequired(true)
-        )
     ),
+  // .addSubcommand(cmd =>
+  //   cmd
+  //     .setName('view')
+  //     .setDescription('View a specific character')
+  //     .addIntegerOption(opt =>
+  //       opt
+  //         .setName('localid')
+  //         .setDescription(
+  //           'The character local id, aka its position in the list'
+  //         )
+  //         .setMinValue(1)
+  //         .setRequired(true)
+  //     )
+  // ),
   execute: async interaction => {
     const cmd = interaction.options.getSubcommand();
 
     if (cmd === 'all') {
       await listAll(interaction);
-      return;
     }
-    // } else if (cmd === 'view') {
-    //   await listView(interaction);
-    //   return;
-    // }
-
-    // should be unreachable
-    await interaction.reply('Unknown command provided');
   },
 };
 
