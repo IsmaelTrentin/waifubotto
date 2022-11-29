289d7c6bd32cbf68240e72b95403655e0b7ecbe8
Ismael Trentin <ismael.trentin@samtrevano.ch>
2022-11-29 01:34:53 +0100

    create consts file

---
+++ apps/bot/src/utils/constants.ts
@@ -0,0 +1,9 @@
+export const MAX_ITEMS_IN_LIST = 3;
+
+export const BTN_INTERACTIONS_IDS = {
+  listall: {
+    $cmd: 'list.all',
+    prev: 'list.all_r0_b_prev',
+    next: 'list.all_r0_b_next',
+  },
+};
